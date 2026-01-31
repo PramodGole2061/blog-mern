45a4c5fa487e33f932908cb8cf8d33c38d2f2099
PramodGole2061 <golepramod72@gmail.com>
2026-01-31 13:04:00 +0545

    connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import dbConnection from './config/dbConnection.js';
+
+//this will make dotenv.config() to be global on all files
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+//connect to db
+dbConnection().then(()=>{
+    app.listen(3000, ()=>{
+        console.log("Server is running on port 3000");
+    });
+}).catch((error)=>{
+    console.error("Failed to start server due to DB connection error", error);
 })
