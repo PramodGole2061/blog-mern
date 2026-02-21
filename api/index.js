74953f4016db94467f9c52d19aba66d26767c354
PramodGole2061 <golepramod72@gmail.com>
2026-02-21 16:58:34 +0545

    fix error on pre installation

--- api/index.js
+++ api/index.js
@@ -47,7 +47,7 @@ app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res)=>{
+app.get('*all', (req, res)=>{
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
